5c468194f5a8b34800fbf0464f6fa0a765ea6837
Zhihao Liu <zhihaliu@amazon.com>
2020-11-24 17:04:05 -0800

    Correct comment

--- sample_apps/python/CrudAndSimpleIngestionExample.py
+++ sample_apps/python/CrudAndSimpleIngestionExample.py
@@ -247,7 +247,7 @@ class CrudAndSimpleIngestionExample:
             print("Error:", err)
 
 
-        # upsert with higher version as new data iss generated
+        # upsert with higher version as new data is generated
         version = int(self._current_milli_time())
         common_attributes["Version"] = version
 
