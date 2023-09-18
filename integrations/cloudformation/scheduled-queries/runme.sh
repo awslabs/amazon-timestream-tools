996ae33a25158226e704007f3bc1033c67b22b7f
Norbert Funke <funknor@amazon.com>
2023-09-18 10:52:11 -0400

    cleaning up yaml and runme for generic usage

--- integrations/cloudformation/scheduled-queries/runme.sh
+++ integrations/cloudformation/scheduled-queries/runme.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
-aws s3 cp scheduled_query.yaml s3://funknor-s3-upload/cfn/
-aws cloudformation delete-stack --stack-name myteststack --region us-east-1
+S3BUCKET=$1
+
+if [ -z "$S3BUCKET" ]
+then
+	echo "Please specify S3 bucket name for uploading cloudformation template"
+	echo "usage:"
+	echo "sh ./runme.sh <s3 bucket name>"
+	exit -1
+fi
+
+
+aws s3 cp scheduled_query.yaml s3://${S3BUCKET}/cfn/
+# aws cloudformation delete-stack --stack-name scheduled-query-example --region us-east-1
 aws cloudformation create-stack \
   --capabilities CAPABILITY_IAM \
   --region us-east-1 \
-  --stack-name myteststack \
-  --template-url https://funknor-s3-upload.s3.amazonaws.com/cfn/scheduled_query.yaml #\
+  --stack-name scheduled-query-example \
+  --template-url https://${S3BUCKET}.s3.amazonaws.com/cfn/scheduled_query.yaml #\
 #  --parameters ParameterKey=Parm1,ParameterValue=test1 ParameterKey=Parm2,ParameterValue=test2
