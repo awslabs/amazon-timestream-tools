3810228b19e6b530dd3ac1fb5f12eadaf0a56348
Norbert Funke <funknor@amazon.com>
2024-01-29 19:16:02 -0500

    added WHERE clause

--- sample_apps/sql/join_tables/query_example_join_tables.sql
+++ sample_apps/sql/join_tables/query_example_join_tables.sql
@@ -1,5 +1,9 @@
-SELECT events.gpio, temperature, humidity, name, status
+SELECT events.time, events.gpio, temperature, humidity, name, status
 FROM "amazon-timestream-tools"."sensor_events" as events
 JOIN "amazon-timestream-tools"."sensor_details" as details
 on events.gpio = details.gpio
 where details.status = 'active'
+and events.time between '2023-09-19' and '2023-11-01'
+and details.time between '2023-09-19' and '2023-11-01'
+
+-- also add CTE example
