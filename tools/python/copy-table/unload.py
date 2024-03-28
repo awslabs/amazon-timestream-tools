291edf80fe2a5cc99d2fe8b55807c5648b26f531
Francesco Martini <ffmarti@amazon.com>
2024-03-28 23:11:52 +0100

    support for py 3.9 + partitioning optional + improved readme

--- tools/python/copy-table/unload.py
+++ tools/python/copy-table/unload.py
@@ -7,7 +7,7 @@ from botocore.config import Config
 from utils.logger_utils import create_logger
 from utils.s3_utils import s3Utility
 
-def main(logger, region, database, table, bucket_s3_uri, from_time, end_time, partition, iam_role_bucket_policy):
+def main(logger, region, database, table, bucket_s3_uri, from_time, end_time, partition, iam_role_arn_bucket_policy):
 
     session = boto3.Session()
     if (region is None or len(region) == 0):
@@ -29,22 +29,22 @@ def main(logger, region, database, table, bucket_s3_uri, from_time, end_time, pa
         bucket_s3_uri = s3_utility.create_s3_bucket(bucket_name)
 
     # Create bucket policy for accessing data if IAM Role is provided 
-    if (iam_role_bucket_policy):
+    if (iam_role_arn_bucket_policy):
         bucket_name = bucket_s3_uri.split('s3://')[1]
         bucket_name = bucket_name.split('/')[0]
         
         bucket_policy = {
             'Version': '2012-10-17',
             'Statement': [{
-                'Sid': 'PermissionS3Copy1',
+                'Sid': 'PermissionS3CopyGetObj',
                 'Effect': 'Allow',
-                'Principal': {'AWS': f'{iam_role_bucket_policy}'},
+                'Principal': {'AWS': f'{iam_role_arn_bucket_policy}'},
                 'Action': ['s3:GetObject'],
                 'Resource': f'arn:aws:s3:::{bucket_name}/*'
             },{
-                'Sid': 'PermissionS3Copy2',
+                'Sid': 'PermissionS3CopyListBucket',
                 'Effect': 'Allow',
-                'Principal': {'AWS': f'{iam_role_bucket_policy}'},
+                'Principal': {'AWS': f'{iam_role_arn_bucket_policy}'},
                 'Action': ['s3:ListBucket'],
                 'Resource': f'arn:aws:s3:::{bucket_name}'
             }
@@ -62,13 +62,12 @@ def build_query(database, table, bucket_s3_uri, from_time, end_time, partition):
     unload_query = "UNLOAD("
     unload_query += " SELECT *, to_nanoseconds(time) as nanoseconds"
     if (partition):
-        match partition:
-            case "day":
-                 unload_query += ", DATE_FORMAT(time,'%Y-%m-%d') as partition_date"
-            case "month":
-                unload_query += ", DATE_FORMAT(time,'%Y-%m') as partition_date"
-            case "year":
-                unload_query += ", DATE_FORMAT(time,'%Y') as partition_date"  
+        if (partition == "day"):
+            unload_query += ", DATE_FORMAT(time,'%Y-%m-%d') as partition_date"
+        elif (partition == "month"):
+            unload_query += ", DATE_FORMAT(time,'%Y-%m') as partition_date"
+        elif (partition == "year"):
+            unload_query += ", DATE_FORMAT(time,'%Y') as partition_date"  
     unload_query += " FROM " + database + "." + table
     if (from_time and end_time):
         unload_query += " WHERE time >= '" + from_time + "' AND time < '" + end_time + "'"
@@ -80,7 +79,9 @@ def build_query(database, table, bucket_s3_uri, from_time, end_time, partition):
     unload_query += " time asc )"
         
     unload_query += " TO '" + bucket_s3_uri + "'"
-    unload_query += " WITH (partitioned_by = ARRAY['partition_date'],"
+    unload_query += " WITH ("
+    if (partition):
+        unload_query += " partitioned_by = ARRAY['partition_date'],"
     unload_query += " format='csv',"
     unload_query += " compression='none',"
     unload_query += " max_file_size = '4.9GB',"
@@ -111,8 +112,8 @@ if __name__ == '__main__':
     parser.add_argument("-s", "--s3_uri", help="S3 Bucket URI to store unload data", required=False)
     parser.add_argument("-f", "--from_time", help="Timestamp from which you want to unload data (included)", required=False)
     parser.add_argument("-e", "--end_time", help="Timestamp to which you want to unload data (not included)", required=False)
-    parser.add_argument("-p", "--partition", help="Partition data by 'day', 'month' or 'year'", required=True, choices=['day', 'month', 'year'])
-    parser.add_argument("-i", "--iam_role_bucket_policy", help="S3 Bucket policy to apply to the S3 Bucket where unload data is stored", required=False)
+    parser.add_argument("-p", "--partition", help="Partition data by 'day', 'month' or 'year'", required=False, choices=['day', 'month', 'year'])
+    parser.add_argument("-i", "--iam_role_arn_bucket_policy", help="IAM Role ARN used in the S3 Bucket policy that is applied to the S3 Bucket where unload data is stored", required=False)
    
     #assign arguments to args variable
     args = parser.parse_args()
@@ -120,7 +121,7 @@ if __name__ == '__main__':
     #create logger
     logger = create_logger("Unload Logger")
 
-    main(logger, args.region, args.database, args.table, args.s3_uri, args.from_time, args.end_time, args.partition, args.iam_role_bucket_policy)
+    main(logger, args.region, args.database, args.table, args.s3_uri, args.from_time, args.end_time, args.partition, args.iam_role_arn_bucket_policy)
 
     logger.info("COMPLETED SUCCESSFULLY")
 
